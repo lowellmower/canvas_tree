c5049dec26ea4350f4a24e13cb39624496f5b4af
Lowell Mower <lowell.m.mower@gmail.com>
2015-11-13 14:07:53 -0500

    initial commit with html/js/jquery files

---
+++ tree.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  var canvas = document.querySelector('canvas');
+  var context = canvas.getContext("2d");
+
+  context.fillStyle = 'red';
+  context.fillRect(10,10,100,50);
+});
